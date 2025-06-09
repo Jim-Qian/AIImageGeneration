d0cc7d92419695c5e6dfbd266364047da5b4cbf4
JimQian2023 <122236062+JimQian2023@users.noreply.github.com>
2025-06-08 19:04:24 -0700

    Updated launch.json so it can run Next.js Server OR Flask/Python depending on which part you comment out. Also added config.json to Backend/.

--- Backend/main.py
+++ Backend/main.py
@@ -4,16 +4,25 @@ For debugging only.
 
 from DB.SQLite_Manager import SQLite_Manager
 from DB.User import User
+from APIs import OpenAI_Client
 
 
-sqlManager = SQLite_Manager() 
+def testSQLManager():
+    sqlManager = SQLite_Manager() 
 
-sqlManager.createUsersTable()
-user1 = User("Jim", "AdminJim2025Admin", 100)
-user1.makeAdmin()
-user1.insertIntoDB(sqlManager)
-user2 = User("Guest", "Guest", 0)
-user2.insertIntoDB(sqlManager)
+    sqlManager.createUsersTable()
+    user1 = User("Jim", "AdminJim2025Admin", 100)
+    user1.makeAdmin()
+    user1.insertIntoDB(sqlManager)
+    user2 = User("Guest", "Guest", 0)
+    user2.insertIntoDB(sqlManager)
 
-sqlManager.getAllRows("Users")
+    sqlManager.getAllRows("Users")
+    pass
+
+def testOpenAIClient():
+    OpenAI_Client.test()
+    pass
+
+testOpenAIClient()
 pass
