aeea26801a6a4e73e3e7544e9808cdf74dafb506
JimQian2023 <122236062+JimQian2023@users.noreply.github.com>
2025-07-10 22:10:32 -0700

    .

--- Scripts/start-backend.sh
+++ Scripts/start-backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/ec2-user/AIImageGeneration/venv/bin/activate
+source /home/ec2-user/AIImageGeneration/env/bin/activate
 export PYTHONPATH=Backend
 export FLASK_APP=controller2
 flask run --host=0.0.0.0
